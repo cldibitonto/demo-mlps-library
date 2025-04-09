d1e47b2212f4a289fe65deca62464dbcfb4b5da1
DESKTOP-ODU9K7N\Edynet-Penta <ermes.diprisco@penta-si.it>
2025-04-09 16:24:28 +0200

    primo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MLPSWizardComponent, TabItem, TestComponent} from 'mlps-template'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TestComponent, MLPSWizardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'lib-test';
+
+  tabList: TabItem[] = [{
+    name: 'App',
+    component: TestComponent,
+    showCheck: true,
+    showWarning: true
+  }, 
+    {
+      name: 'app2',
+      component: TestComponent
+    }]
 }
