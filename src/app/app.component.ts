91d3152baed2fb163ae7731f406d3367521404b5
Erika Guasconi <Erika.Guasconi@it.ey.com>
2025-04-10 15:22:50 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MLPSFooterComponent, MLPSHeaderComponent} from 'mlps-template'
-import { BreadcrumbComponent } from "./components/breadcrumb/breadcrumb.component";
+import { MLPSBreadcrumbComponent, MLPSFooterComponent, MLPSHeaderComponent} from 'mlps-template'
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ RouterOutlet, MLPSHeaderComponent,  MLPSFooterComponent, BreadcrumbComponent],
+  imports: [ RouterOutlet, MLPSHeaderComponent,  MLPSFooterComponent, MLPSBreadcrumbComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
