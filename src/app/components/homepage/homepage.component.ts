d1012da16bb4d4f3d99fa1c2690550d6329ed491
Erika Guasconi <Erika.Guasconi@it.ey.com>
2025-04-17 11:17:21 +0200

    notification

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component, inject} from '@angular/core';
 import { Router } from '@angular/router';
 import { LoaderComponent } from "../loader/loader.component";
 import { CarouselComponent } from '../carousel/carousel.component';
+import { NotificationComponent } from '../notification/notification.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [LoaderComponent, CarouselComponent],
+  imports: [LoaderComponent, CarouselComponent, NotificationComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
---
+++ src/app/components/notification/notification.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { MLPSNotifica, MLPSNotificationComponent } from 'mlps-template';
+
+@Component({
+  selector: 'app-notification',
+  standalone: true,
+  imports: [MLPSNotificationComponent],
+  templateUrl: './notification.component.html',
+  styleUrl: './notification.component.scss'
+})
+export class NotificationComponent {
+  notifiche: MLPSNotifica[] = [
+    {
+      titolo: 'Notifica 1',
+      contenuto: 'Contenuto 1',
+      data: '10/04/2025',
+      isLetto: true
+    },
+    {
+      titolo: 'Notifica 2',
+      contenuto: 'Contenuto 2',
+      data:'10/04/2025',
+      isLetto: false
+    }
+  ];
+  
+  segnaComeLetta(notifica: MLPSNotifica) {
+    notifica.isLetto = true;
+  }
+  
+  segnaTutte() {
+    this.notifiche.forEach(n => n.isLetto = true);
+  }
+  
+}
